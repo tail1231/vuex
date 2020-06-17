9b3a915a7c16458ecf6662af239db01f6a7b1fd5
chenmingyu <cdy1571@163.com>
2020-06-17 23:03:04 +0800

    cmy

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: [], //所有的任务列表
+    inputValue: 'aaa' //文本框的内容
   },
   mutations: {
+    initList(state, list) {
+      state.list = list
+    },
+    changeInputValue(state, val) {
+      console.log('changeInputValue', val)
+      state.inputValue = val
+    }
   },
   actions: {
+    getList(context) {
+      axios.get('/list.json').then(({ data }) => {
+        console.log(data)
+        context.commit('initList', data)
+      })
+    }
   },
-  modules: {
+  modules: {},
+  getters: {
+    showList(state) {
+      return state.list
+    }
   }
 })
