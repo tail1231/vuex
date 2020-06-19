d140b87a4a3f37ae6dc97d579d205ba22f779efe
chenmingyu <cdy1571@163.com>
2020-06-19 23:44:36 +0800

    tj

--- src/App.vue
+++ src/App.vue
@@ -6,35 +6,55 @@
       :value="inputValue"
       @change="handleInputChange"
     />
-    <a-button type="primary">添加事项</a-button>
+    <a-button type="primary" @click="handleAddItem">添加事项</a-button>
 
-    <a-list bordered :dataSource="list" class="dt_list">
+    <a-list bordered :dataSource="infoList" class="dt_list">
       <a-list-item slot="renderItem" slot-scope="item">
         <!-- 复选框 -->
-        <a-checkbox>{{ item.info }}</a-checkbox>
+        <a-checkbox
+          @change="
+            e => {
+              handleCheckClick(e, item.id)
+            }
+          "
+          :checked="item.done"
+          >{{ item.info }}</a-checkbox
+        >
         <!-- 删除链接 -->
-        <a slot="actions">删除</a>
+        <a slot="actions" @click="handleRemove(item.id)">删除</a>
       </a-list-item>
 
       <!-- footer区域 -->
       <div slot="footer" class="footer">
         <!-- 未完成的任务个数 -->
-        <span>0条剩余</span>
+        <span>{{ unDoneLength }}条剩余</span>
         <!-- 操作按钮 -->
         <a-button-group>
-          <a-button type="primary">全部</a-button>
-          <a-button>未完成</a-button>
-          <a-button>已完成</a-button>
+          <a-button
+            :type="viewVal == 'all' ? 'primary' : 'default'"
+            @click="handleBtnClick('all')"
+            >全部</a-button
+          >
+          <a-button
+            :type="viewVal == 'unDone' ? 'primary' : 'default'"
+            @click="handleBtnClick('unDone')"
+            >未完成</a-button
+          >
+          <a-button
+            :type="viewVal == 'done' ? 'primary' : 'default'"
+            @click="handleBtnClick('done')"
+            >已完成</a-button
+          >
         </a-button-group>
         <!-- 把已经完成的任务清空 -->
-        <a>清除已完成</a>
+        <a @click="removeAllDone">清除已完成</a>
       </div>
     </a-list>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapState , mapMutations } from 'vuex'
+import { mapGetters, mapState, mapMutations } from 'vuex'
 export default {
   name: 'app',
   data() {
@@ -44,14 +64,53 @@ export default {
     this.$store.dispatch('getList')
   },
   computed: {
-    // ...mapGetters(['showList'])
-    ...mapState(['list', 'inputValue'])
+    ...mapGetters(['unDoneLength', 'infoList']),
+    ...mapState(['list', 'inputValue', 'viewVal'])
   },
   methods: {
-    // ...mapMutations(['changeInputValue']),
+    ...mapMutations([
+      'changeInputValue',
+      'addItemToList',
+      'removeItem',
+      'checkboxChangeHandle',
+      'clean',
+      'changeViewValue'
+    ]),
     handleInputChange(e) {
-      this.$store.commit('changeInputValue',e.target.value)
+      this.$store.commit('changeInputValue', e.target.value)
       console.log(e.target.value)
+    },
+    //向列表中新增item项
+    handleAddItem() {
+      if (this.inputValue.trim().length <= 0) {
+        return this.$message.warning('文本框内容不能为空！')
+      } else {
+        this.addItemToList()
+      }
+    },
+    //删除
+    handleRemove(id) {
+      this.removeItem(id)
+    },
+    //点击复选框
+    handleCheckClick(e, id) {
+      const {
+        target: { checked }
+      } = e
+      let obj = {
+        checked: checked,
+        id: id
+      }
+      this.checkboxChangeHandle(obj)
+    },
+    //清除已完成
+    removeAllDone() {
+      this.clean()
+    },
+    //按钮点击
+    handleBtnClick(val) {
+      // this.$store.dispatch('getList')
+      this.changeViewValue(val)
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,9 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     list: [], //所有的任务列表
-    inputValue: 'aaa' //文本框的内容
+    inputValue: 'aaa', //文本框的内容
+    nextId: 5, //下一个id
+    viewVal: 'all'
   },
   mutations: {
     initList(state, list) {
@@ -16,6 +18,48 @@ export default new Vuex.Store({
     changeInputValue(state, val) {
       console.log('changeInputValue', val)
       state.inputValue = val
+    },
+    //添加列表项
+    addItemToList(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputValue = ''
+    },
+    removeItem(state, i) {
+      const o = state.list.findIndex(x => x.id == i)
+      if (o != -1) {
+        state.list.splice(o, 1)
+      }
+    },
+    checkboxChangeHandle(state, obj) {
+      const i = state.list.findIndex(x => x.id == obj.id)
+      if (i != -1) {
+        state.list[i].done = obj.checked
+      }
+    },
+    clean(state) {
+      state.list = state.list.filter(x => x.done === false)
+    },
+    changeViewValue(state, val) {
+      console.log(state, val)
+      state.viewVal = val
+      switch (val) {
+        case 'all':
+          console.log(this);
+          this.dispatch('getList')
+          break
+        case 'unDone':
+          state.list = state.list.filter(x => x.done === false)
+          break
+        case 'done':
+          state.list = state.list.filter(x => x.done === true)
+          break
+      }
     }
   },
   actions: {
@@ -29,6 +73,21 @@ export default new Vuex.Store({
   modules: {},
   getters: {
     showList(state) {
+      return state.list
+    },
+    unDoneLength(state) {
+      return state.list.filter(x => x.done === false).length
+    },
+    infoList(state) {
+      if (state.viewVal === 'all') {
+        return state.list
+      }
+      if (state.viewVal === 'unDone') {
+        return state.list.filter(x => x.done === false)
+      }
+      if (state.viewVal === 'done') {
+        return state.list.filter(x => x.done === true)
+      }
       return state.list
     }
   }
